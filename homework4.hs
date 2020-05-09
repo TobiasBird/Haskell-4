0b7de9b149a727fdd8dfee2d095b36a1fe52283f
Tobias Bird <birdto@flip1.engr.oregonstate.edu>
2020-05-09 15:39:42 -0700

    Tried working on exercise 2.a but its not working

--- homework4.hs
+++ homework4.hs
@@ -1,1 +1,20 @@
---Group Members: Harper Swenson, Tobias Bird, Victor Campa, Gregory Oertli--
+-- Group Members: Harper Swenson, Tobias Bird, Victor Campa, Gregory Oertli --
+--
+-- exercise 1 --
+
+
+-- exercise 2 --
+data Shape = X
+	   | TD Shape Shape
+	   | LR Shape Shape
+	   deriving Show
+
+type BBox = (Int,Int)
+
+-- a --
+--data Type = Int
+	    --deriving (Eq, Show)
+bbox :: Shape -> BBox
+bbox X = (1,1)
+bbox (TD s1 s2) | bbox s1==BBox && bbox s2==BBox = BBox
+bbox (LR s1 s2) | bbox s1==BBox && bbox s2==BBox = BBox
