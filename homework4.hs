864b205382536fab61abb86c57b3074830e6383e
harperSwenson <56015648+harperSwenson@users.noreply.github.com>
2020-05-12 20:01:59 -0700

    Update homework4.hs

--- homework4.hs
+++ homework4.hs
@@ -12,9 +12,18 @@ data Shape = X
 type BBox = (Int,Int)
 
 -- a --
---data Type = Int
-	    --deriving (Eq, Show)
+
+maxx :: Int -> Int -> Int
+maxx x1 x2 = if x1>x2 then x1 else x2
+
+maxy :: Int -> Int -> Int
+maxy y1 y2 = if y1>y2 then y1 else y2
+
 bbox :: Shape -> BBox
 bbox X = (1,1)
-bbox (TD s1 s2) | bbox s1==BBox && bbox s2==BBox = BBox
-bbox (LR s1 s2) | bbox s1==BBox && bbox s2==BBox = BBox
+bbox (TD s1 s2) = ((maxx x1 x2), y1 + y2)
+				where (x1,y1) = bbox s1
+					  (x2,y2) = bbox s2
+bbox (LR s1 s2) = ((x1 + x2), maxy y1 y2)
+				where (x1,y1) = bbox s1
+					  (x2,y2) = bbox s2
